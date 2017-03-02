7aa907ded6e7d6a09c324e46e0dd094c17e78a7d
Joël Luijmes <joelluijmes@gmail.com>
2017-03-02 16:49:31 +0100

    Created UUID class

---
+++ Bluetooth/UUID.cpp
@@ -0,0 +1,61 @@
+﻿#include "UUID.h"
+
+#include <cstring>
+#include <cstdio>
+
+namespace Bluetooth
+{
+	UUID::UUID() : m_ShortUUID(-1)
+	{
+		memset(m_UUID, -1, sizeof(m_UUID));
+	}
+
+	UUID::UUID(uint8_t longUUID[]) : m_ShortUUID(-1)
+	{
+		memcpy(m_UUID, longUUID, sizeof(m_UUID));
+	}
+
+	UUID::UUID(uint16_t shortUUID) : m_ShortUUID(shortUUID)
+	{
+		// 128_bit_value = 16_bit_value * 2^96 + Bluetooth_Base_UUID
+		// Or, to put it more simply, the 16-bit Attribute UUID replaces the x’s in the follow- ing:
+		// 0000xxxx - 0000 - 1000 - 8000 - 00805F9B34FB
+
+		m_UUID[0] = 0x00;
+		m_UUID[1] = 0x00;
+		m_UUID[2] = (shortUUID >> 8) & 0xFF;
+		m_UUID[3] = (shortUUID) & 0xFF;
+		m_UUID[4] = 0x00;
+		m_UUID[5] = 0x00;
+		m_UUID[6] = 0x10;
+		m_UUID[7] = 0x00;
+		m_UUID[8] = 0x80;
+		m_UUID[9] = 0x00;
+		m_UUID[10] = 0x00;
+		m_UUID[11] = 0x80;
+		m_UUID[12] = 0x5F;
+		m_UUID[13] = 0x9B;
+		m_UUID[14] = 0x34;
+		m_UUID[15] = 0xFB;
+	}
+
+	void UUID::ToCharArray(char* buf, uint8_t len) const
+	{
+		char tmp[37] = { 0 };
+		snprintf(tmp, sizeof(tmp) - 1, "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
+			m_UUID[0], m_UUID[1], m_UUID[2], m_UUID[3], m_UUID[4], m_UUID[5], m_UUID[6], m_UUID[7],
+			m_UUID[8], m_UUID[9], m_UUID[10], m_UUID[11], m_UUID[12], m_UUID[13], m_UUID[14], m_UUID[15]);
+
+		strncpy(buf, tmp, len);
+	}
+
+	uint16_t UUID::GetShortUUID() const
+	{
+		return m_ShortUUID;
+	}
+
+	const uint8_t* UUID::GetLongUUID() const
+	{
+		return m_UUID;
+	}
+}
---
+++ Bluetooth/UUID.h
@@ -0,0 +1,61 @@
+﻿#ifndef UUID_H_
+#define UUID_H_
+
+#include <inttypes.h>
+
+namespace Bluetooth
+{
+	class UUID
+	{
+	public:
+		/// 
+		/// Constructs UUID with 0xFF as content
+		/// 
+		UUID();
+
+		/// 
+		/// Constructs a UUID from the long 128 bit version.
+		///
+		/// @param longUUID		Long 128 bit UUID
+		/// 
+		explicit UUID(uint8_t longUUID[]);
+
+		/// 
+		/// Constructs a UUID from a short version. Internally it creates the 128 bit format
+		/// by using the Bluetooth_Base_UUID: 0000xxxx - 0000 - 1000 - 8000 - 00805F9B34FB
+		///
+		/// @param shortUUID		Short UUID to construct with
+		/// 
+		explicit UUID(uint16_t shortUUID);
+
+
+		/// 
+		/// Gets the short 16 - bit UUID, only valid when constructed from UUID(uint16_t)
+		///
+		/// @return				the short UUID
+		/// 
+		uint16_t GetShortUUID() const;
+
+		/// 
+		/// Gets the long 128 bit UUID.
+		///
+		/// @return				pointer to the internal UUID
+		/// 
+		const uint8_t* GetLongUUID() const;
+
+		/// 
+		/// Formats it to a UUID, buffer should be at least 37 characters to store
+		/// the whole string.
+		///
+		/// @param buf		Buffer to store the UUID
+		/// @param len		Length of the buffer
+		/// 
+		void ToCharArray(char* buf, uint8_t len) const;
+
+	private:
+		uint8_t m_UUID[16];
+		uint16_t m_ShortUUID;
+	};
+}
+
+#endif // !UUID_H_
