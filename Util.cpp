011347a0b8757440c934c8e06b208b5e4b3b4ff6
Joël Luijmes <joelluijmes@gmail.com>
2017-03-02 08:40:46 +0100

    added the Util::ParseMacAddress

---
+++ Util.cpp
@@ -0,0 +1,19 @@
+﻿#include "Util.h"
+
+#include <stdlib.h>
+
+void Util::Parse6ByteMACAddressFromString(const char* str, uint8_t macAddress[])
+{
+	char tmp[3] = { 0 };
+	for (uint8_t i = 0; i < 6; ++i)
+	{
+		// read the two hex nybbles
+		tmp[0] = str[0];
+		tmp[1] = str[1];
+
+		unsigned long val = strtoul(tmp, NULL, 16);
+		macAddress[i] = static_cast<uint8_t>(val);
+
+		str += 2;
+	}
+}
---
+++ Util.h
@@ -0,0 +1,12 @@
+﻿#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <inttypes.h>
+
+class Util
+{
+public:
+	static void Parse6ByteMACAddressFromString(const char* str, uint8_t macAddress[]);
+};
+
+#endif // !UTIL_H_
